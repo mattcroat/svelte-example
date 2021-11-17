ef99898825c2b6dcc65f94b1e2c8629c32659c93
mattcroat <mattcroat.dev@gmail.com>
2021-11-17 22:48:17 +0100

    feat: Add Svelte

---
+++ src/Nested.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  type Answer = number | string
+  export let answer: Answer = 'a mystery'
+</script>
+
+<p>The answer is {answer}</p>
---
+++ src/Thing.svelte
@@ -0,0 +1,16 @@
+<script>
+  const emojis = {
+    apple: '🍎',
+    banana: '🍌',
+    carrot: '🥕',
+    doughnut: '🍩',
+    egg: '🥚',
+  }
+
+  export let name
+  let emoji = emojis[name]
+</script>
+
+<p>
+  <span>The emoji for {name} is {emoji}</span>
+</p>
